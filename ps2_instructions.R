25419e063dc844dea6120227f70b373dc9a4a1d0
Evan Fricke <evanfricke@gmail.com>
2021-02-11 16:26:40 -0800

    Adding instructions

---
+++ ps2_instructions.R
@@ -0,0 +1,44 @@
+# EBIO338/358: Analysis and Visualization of Biological Data
+# Problem Set 2
+
+# 1) Load the stream flow data and call it flow. Examine the structure.
+
+# 2) Create a contingency table showing the number of observations per year for each stream
+
+
+# 3 a) Plot maximum flow as a function of elevation. 
+
+# b) Make the axes lables easier to read 
+
+# c) Add a title.
+
+# d) Rotate the axis labels to that the y-axis labels are horizontal and the x-axis labels are vertical
+
+# e) Color code the points based on stream name and points for each stream a unique, filled point type
+
+# f) Add a legend to the upper right corner with color coded text 
+
+# g) and a coresponding point
+
+
+# 4 a) Create a horizontal boxplot showing elevation for each stream sampled with easily readable axes
+
+# b) Make the width of the boxes proportional to the number of observations per stream
+
+# c) Make the color of each boxplot correspond to its colors from #3e
+
+
+# 5 a) Create a horizontal barplot of aspect for each stream flow sampling site with readable x, y lables and title
+
+# b) Make all of the individual bar labels visible and remove bar borders
+
+# c) Order the bars from the highest (top) to the lowest (bottom) aspect
+
+# d) Ensure the labels exceed the maximum aspect value
+
+# e) Add your name in magenta bold text centered above the main title
+
+
+# 6) Create a scatterplot or boxplot to examine the relationship among
+# variables that differ from those examined above, modifying at least 4 
+# arguments to create a more informative or attractive plot.
